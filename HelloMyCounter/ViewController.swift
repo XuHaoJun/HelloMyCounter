144644ef832dbc53cb1824aa25c48c1e7ea50fc5
fguai <fguai@fguais-iMac-9.local>
2017-06-22 15:52:37 +0800

    First commit.

--- HelloMyCounter/ViewController.swift
+++ HelloMyCounter/ViewController.swift
@@ -7,12 +7,24 @@
 //
 
 import UIKit
+import SwiftMoment
+
+let locale = Locale(identifier: "zh-tw")
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var logTextView: UITextView!
+    @IBOutlet weak var adultCountLable: UILabel!
+    @IBOutlet weak var childCountLabel: UILabel!
+    
+    var adultDates = [Date]()
+    var childDates = [Date]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        adultCountLable.text = "0"
+        childCountLabel.text = "0"
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +32,37 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func adlutBtnPress(_ sender: Any) {
+        let now = Date()
+        adultDates.append(now)
+        adultCountLable.text = "\(adultDates.count)"
+    }
+    
+    @IBAction func childBtnPress(_ sender: Any) {
+        let now = Date()
+        childDates.append(now)
+        childCountLabel.text = "\(childDates.count)"
+    }
+    
+    func dateToLocalTimeString(date: Date) -> String {
+        let formatter = DateFormatter()
+        formatter.timeZone = TimeZone.current
+        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        return formatter.string(from:date)
+    }
+    
+    @IBAction func historyBtnPress(_ sender: Any) {
+        var logText = ""
+        logText = "＊ 小孩進場時間：\n"
+        for date in childDates {
+            logText = "\(logText)\(dateToLocalTimeString(date: date))\n"
+        }
+        logText = "\(logText)\n"
+        logText = "\(logText)＊ 大人進場時間：\n"
+        for date in adultDates {
+            logText = "\(logText)\(moment(date).fromNow())\n"
+        }
+        logTextView.text = logText
+    }
 }
 
